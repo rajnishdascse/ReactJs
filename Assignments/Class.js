1a784d73aa984b595ec8906f1edcf3b2ee473472
Rajnish <45910029+rajnishdascse@users.noreply.github.com>
2021-09-26 12:43:27 +0530

    Day1 Assignment
    There are 3 components, one is class-based and the rest is functional-based components. 
    Functional-Based components are-  Title.js and Profile.js
    Class_based component is - Class.js

---
+++ Assignments/Class.js
@@ -0,0 +1,12 @@
+import react, {Component} from 'react';
+class Class extends Component{
+    render(){
+        return(
+            <div>
+                <h2>This one is Class based component</h2>
+            </div>
+        )
+    }
+}
+
+export default Class;
---
+++ Assignments/Title.js
@@ -0,0 +1,10 @@
+import react from 'react';
+function Title(){
+    return (
+        <div>
+            <h1>Hey, This is Rajnish</h1>
+        </div>
+    )
+}
+
+export default Title;
