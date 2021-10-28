a47e998c4dc4a01b98aa8d8278e084c2714d62d5
Rajnish <45910029+rajnishdascse@users.noreply.github.com>
2021-10-28 20:11:27 +0530

    Update App.js

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,19 +1,8 @@
-//import React from 'react'
-//import menuCard from "./Components/Basics/MenuCard";
-//import Restaurant from "./Components/Basics/Restaurant";
-//import UseState from "./Components/Hooks/useState";
-//import UseEffect from "./Components/Hooks/UseEffect";
-//import UseRedux from "./Components/Hooks/UseReducer";
 import Todo from "./Components/todoList/todolist";
 
 export const App = () => {
   return (
     <div>
-      {/*<Restaurant/>*/}
-      {/*<UseState/> */}
-      {/*<UseEffect/>*/}
-      {/*<UseRedux/>*/}
-
       <Todo/>
 
     </div>
